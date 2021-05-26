06e50e0c8c265e75a6c34f13a1c5c05de46e64e3
Ginger-a <60004141+Ginger-a@users.noreply.github.com>
2021-05-26 12:15:45 -0500

    iniciando el tema

---
+++ front-page.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+<main>
+<title>My Blog</title>
+<div class="flex items-center  space-x-10">
+<div class="w-1/12">
+<img 
+class="rounded-full w-1/12" 
+src="<?php echo get_template_directory_uri()?>/assets/img/ginger.jpeg" 
+alt="imagen de perfil de Gio"
+>
+</div>
+<div class="w-6/12">
+<p>My personal blog  Ginger Muñoz naci el 4 de mayo tengo 21 años soy estudiante del Instituto Tecnologico Quito de la carrera analisis de sistemas my hobby es escuchar musica bailar mis propisitos es llegar hacer una gran profesional y seguir ejerciendo en mi carrera y tener nuevos conocimientos</p>
+</div>
+</div>
+</main>
+<?php get_footer(); ?>
